6ebb3976df27e54fb7eceddf9141ee4ab3bd8f07
PancakeHat <pancakehatdev@gmail.com>
2026-03-07 15:08:40 -0600

    begin adding ai

--- src/main.cpp
+++ src/main.cpp
@@ -40,7 +40,9 @@ int staticLevel = 128;
 int power = 100;
 int gameTime = 360;
 
-int location = 3;
+int aiLevel = 1;
+
+int location = 2;
 /*
     classroom - 0
     commons - 1
@@ -133,7 +135,7 @@ int WinMain()
 
 void UpdateUI()
 {
-    if(checkBoxCollison(MousePositionStandard(), {1, 1}, {280, 620}, {720, 100}))
+    if(checkBoxCollison(MousePositionStandard(), {1, 1}, {280, 620}, {720, 100}) && power > 0)
     {
         if(togglingCam == false)
         {
@@ -167,6 +169,60 @@ void UpdateUI()
     if(checkBoxCollison(MousePositionStandard(), {1, 1}, {1096, 540}, {86, 60}) && IsMouseButtonPressed(0)) { camRoom = 2; staticLevel = 230; }
     if(checkBoxCollison(MousePositionStandard(), {1, 1}, {1218, 556}, {32, 44}) && IsMouseButtonPressed(0)) { camRoom = 3; staticLevel = 230; }
     if(checkBoxCollison(MousePositionStandard(), {1, 1}, {1218, 626}, {32, 67}) && IsMouseButtonPressed(0)) { camRoom = 4; staticLevel = 230; }
+}
+
+void AIMove(int newLocation)
+{
+    if(camRoom == newLocation || camRoom == location)
+        staticLevel = 255;
+
+    location = newLocation;
+}
+
+void UpdateAI()
+{
+    if(frames % 420 == 0 && frames != 0)
+    {
+        if(location == 2)
+        {
+            int choice = GetRandomValue(0, 6 - aiLevel);
+
+            if(choice == 0)
+                AIMove(1);
+            else if(choice == 1)
+                AIMove(3);
+        }
+        else if(location == 3)
+        {
+            int choice = GetRandomValue(0, 6 - aiLevel);
+
+            if(choice == 0)
+            {
+                if(aiLevel <= 3)
+                    AIMove(2);
+                else
+                    AIMove(4);
+            }
+            else if(choice == 1)
+                AIMove(4);
+        }
+        else if(location == 1)
+        {
+            int choice = GetRandomValue(0, 7 - aiLevel);
+
+            if(choice == 0)
+            {
+                if(aiLevel <= 3)
+                    AIMove(2);
+                else
+                    AIMove((GetRandomValue(0, 1) == 0) ? 0 : 4);
+            }
+            else if(choice == 1)
+                AIMove(0);
+            else if(choice == 3)
+                AIMove(4);
+        }
+    }
 }
 
 void Update()
@@ -203,33 +259,42 @@ void Update()
         if(staticLevel < 128)
             staticLevel = 128;
     }
-    
+
     // power reduction
-    if(frames % 300 == 0 && power > 0 && frames != 0)
+    if(frames % 300 == 0 && frames != 0)
     {
         if(gameTime > 0)
             gameTime -= 10;
         
-        if(usingCam)
+        if(power > 0)
+        {
+            if(usingCam)
+                power--;
+            if(doorL)
+                power--;
+            if(doorC)
+                power--;
+            if(doorR)
+                power--;
+    
             power--;
-        if(doorL)
-            power--;
-        if(doorC)
-            power--;
-        if(doorR)
-            power--;
-
-        power--;
-
-        if(power < 0)
-            power = 0;
+    
+            if(power <= 0)
+            {
+                power = 0;
+                doorL = false;
+                doorC = false;
+                doorR = false;
+                usingCam = false;
+            }
+        }
     }
 
     // move all world entities
     staticEntities[0].pos.x = -320 + lookAngle;
 
     // door buttons
-    if(!usingCam)
+    if(!usingCam && power > 0)
     {
         if(MouseClickingEntity(staticEntities[1])) { doorL = false; }
         if(MouseClickingEntity(staticEntities[2])) { doorL = true; }
@@ -255,6 +320,7 @@ void Update()
     else if(doorC == false && doorCHeight > 0)
         doorCHeight -= 50;
 
+    UpdateAI();
     UpdateUI();
 }
 
@@ -338,26 +404,29 @@ void Render()
         if(!usingCam)
         {
 
-            DrawSpriteFromVector("hall", {float(-320) + lookAngle, -200}, {1920, 1080}, sprites);
+            DrawSpriteFromVector((power > 0) ? "hall" : "halldark", {float(-320) + lookAngle, -200}, {1920, 1080}, sprites);
             RenderEnemyOffice();
             RenderDoors();
-            DrawAnimFromVector("office", {float(-320) + lookAngle, -200}, {1920, 1080}, anims, sprites, 255);
+            DrawAnimFromVector((power > 0) ? "office" : "officedark", {float(-320) + lookAngle, -200}, {1920, 1080}, anims, sprites, 255);
             
             //door buttons
-            if(doorL)
-                DrawRectOnEntity(staticEntities[2], {255, 100, 100, 180});
-            else
-                DrawRectOnEntity(staticEntities[1], {100, 255, 100, 140});
-            
-            if(doorC)
-                DrawRectOnEntity(staticEntities[4], {255, 100, 100, 180});
-            else
-                DrawRectOnEntity(staticEntities[3], {100, 255, 100, 140});
-            
-            if(doorR)
-                DrawRectOnEntity(staticEntities[6], {255, 100, 100, 180});
-            else
-                DrawRectOnEntity(staticEntities[5], {100, 255, 100, 140});
+            if(power > 0)
+            {
+                if(doorL)
+                    DrawRectOnEntity(staticEntities[2], {255, 100, 100, 180});
+                else
+                    DrawRectOnEntity(staticEntities[1], {100, 255, 100, 140});
+                
+                if(doorC)
+                    DrawRectOnEntity(staticEntities[4], {255, 100, 100, 180});
+                else
+                    DrawRectOnEntity(staticEntities[3], {100, 255, 100, 140});
+                
+                if(doorR)
+                    DrawRectOnEntity(staticEntities[6], {255, 100, 100, 180});
+                else
+                    DrawRectOnEntity(staticEntities[5], {100, 255, 100, 140});
+            }
         }
         else
         {
@@ -370,7 +439,7 @@ void Render()
             else if(camRoom == 3)
                 DrawSpriteFromVector((location == 3) ? "commons1" : "commons", {0, 0}, {1280, 720}, sprites);
             else if(camRoom == 4)
-                DrawSpriteFromVector((location == 0) ? "staircase1" : "staircase", {0, 0}, {1280, 720}, sprites);
+                DrawSpriteFromVector((location == 4) ? "staircase1" : "staircase", {0, 0}, {1280, 720}, sprites);
             
             DrawAnimFromVector("noise", {0, 0}, {1280, 720}, anims, sprites, staticLevel);
         }
@@ -401,17 +470,15 @@ void Render()
                 ImGui::Text(std::format("{}, {}", MousePositionStandard().x, MousePositionStandard().y).c_str());
                 ImGui::Text(std::format("Cam: {}", usingCam).c_str());
                 ImGui::Text(std::format("Noise: {}", staticLevel).c_str());
-
                 ImGui::Text(std::format("L: {}, {} C: {}, {} R: {}, {}", doorL, doorLHeight, doorC, doorCHeight, doorR, doorRHeight).c_str());
+                ImGui::Text(std::format("Game time: {}", gameTime).c_str());
+                ImGui::Text(std::format("Location: {}", location).c_str());
+                ImGui::Text(std::format("AI Level: {}", aiLevel).c_str());
 
-                ImGui::Text(std::to_string(gameTime).c_str());
-                ImGui::Text(std::format("Hrs: {:02d}", (gameTime >= 300) ? 12 : (int)(6 - ceil(gameTime / 60)) - 1).c_str());
-                ImGui::Text(std::format("Min: {:02d}", 60 - (int)(gameTime % 60)).c_str());
-
-                ImGui::SliderInt("X", &tx, 0, 1280);
-                ImGui::SliderInt("Y", &ty, 0, 720);
-                ImGui::SliderInt("W", &tw, 0, 500);
-                ImGui::SliderInt("H", &th, 0, 500);
+                // ImGui::SliderInt("X", &tx, 0, 1280);
+                // ImGui::SliderInt("Y", &ty, 0, 720);
+                // ImGui::SliderInt("W", &tw, 0, 500);
+                // ImGui::SliderInt("H", &th, 0, 500);
                 ImGui::End();
             }
 
